859b7edeb6c6e3f68783c132d825f14f5592144c
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-16 03:04:10 +0300

    Add JsonReader

---
+++ src/OrchardCoreContrib.Localization.Abstractions/CultureDictionaryRecordWrapper.cs
@@ -0,0 +1,17 @@
+﻿using OrchardCore.Localization;
+
+namespace OrchardCoreContrib.Localization
+{
+    public class CultureDictionaryRecordWrapper : CultureDictionaryRecord
+    {
+        public CultureDictionaryRecordWrapper(string messageId, params string[] translations)
+            : this(messageId, null, translations)
+        {
+        }
+
+        public CultureDictionaryRecordWrapper(string messageId, string context, string[] translations)
+            : base(messageId, context, translations)
+        {
+        }
+    }
+}
