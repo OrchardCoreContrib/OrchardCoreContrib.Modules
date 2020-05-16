03cb98c27a567778cd9af853a4d1a41325719b93
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-05-16 09:57:39 +0300

    Add JsonHtmlLocalizer

---
+++ src/OrchardCoreContrib.Localization.Json/JsonHtmlLocalizer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+
+namespace OrchardCoreContrib.Localization.Json
+{
+    /// <summary>
+    /// Represents an <see cref="HtmlLocalizer"/> for JSON.
+    /// </summary>
+    public class JsonHtmlLocalizer : HtmlLocalizer
+    {
+        private readonly IStringLocalizer _localizer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonHtmlLocalizer"/> class.
+        /// </summary>
+        /// <param name="localizer">The <see cref="IStringLocalizer"/>.</param>
+        public JsonHtmlLocalizer(IStringLocalizer localizer) : base(localizer)
+        {
+            _localizer = localizer;
+        }
+
+        /// <inheritdocs />
+        public override LocalizedHtmlString this[string name] => ToHtmlString(_localizer[name]);
+
+        /// <inheritdocs />
+        public override LocalizedHtmlString this[string name, params object[] arguments]
+            => ToHtmlString(_localizer[name], arguments);
+    }
+}
