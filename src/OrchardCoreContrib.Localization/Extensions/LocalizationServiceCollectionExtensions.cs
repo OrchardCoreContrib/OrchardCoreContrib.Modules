a9b444c3968acc74ce4d599e7efbe5353b312e03
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2021-12-29 18:43:12 +0300

    Add AddDiacritics() extension method

---
+++ src/OrchardCoreContrib.Localization/Extensions/LocalizationServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OrchardCoreContrib.Localization.Diacritics;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class LocalizationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDiacritics(this IServiceCollection services)
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddSingleton<IDiacriticsLookup, DiacriticsLookup>();
+
+            return services;
+        }
+    }
+}
---
+++ test/OrchardCoreContrib.Tests/Localization/Diacritics/Extensions/LocalizationServiceCollectionExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCoreContrib.Localization.Diacritics.Tests;
+using Xunit;
+
+namespace OrchardCoreContrib.Localization.Diacritics.Extensions.Tests
+{
+    public class LocalizationServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddDiacriticsShouldAddIDiacriticsLookup()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddDiacritics();
+
+            // Assert
+            var serviceProvider = services.BuildServiceProvider();
+            
+            Assert.NotNull(serviceProvider.GetService<IDiacriticsLookup>());
+        }
+
+        [Fact]
+        public void AddCustomAccentMappersToDI()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddSingleton<IAccentMapper, ArabicAccentMapper>();
+            services.AddDiacritics();
+            services.AddSingleton<IAccentMapper, FrenchAccentMapper>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var lookup = serviceProvider.GetService<IDiacriticsLookup>();
+
+            // Assert
+            Assert.True(lookup.Contains("ar"));
+            Assert.True(lookup.Contains("fr"));
+        }
+    }
+}
