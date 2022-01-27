d60feb2eefb0fe3d5c2e9a79eb98cdfa7664eaff
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-01-27 23:35:13 +0300

    Use PagePreviewMiddleware

---
+++ src/OrchardCoreContrib.ContentPreview/Extensions/PagePreviewOrchardCoreExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Admin;
+using OrchardCore.Environment.Shell;
+using OrchardCoreContrib.ContentPreview;
+using System;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Provides an extension methods for <see cref="IApplicationBuilder"/> to enable page preview.
+    /// </summary>
+    public static class PagePreviewOrchardCoreExtensions
+    {
+        /// <summary>
+        /// Uses the page preview middleware.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        public static IApplicationBuilder UsePagePreview(this IApplicationBuilder app)
+        {
+            if (app is null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            var adminOptions = app.ApplicationServices.GetService<IOptions<AdminOptions>>();
+            var shellFeaturesManager = app.ApplicationServices.CreateScope().ServiceProvider.GetService<IShellFeaturesManager>();
+            var shellSettings = app.ApplicationServices.GetService<ShellSettings>();
+
+            app.UseMiddleware<PagePreviewMiddleware>(adminOptions, shellFeaturesManager, shellSettings);
+
+            return app;
+        }
+    }
+}
--- src/OrchardCoreContrib.ContentPreview/Startup.cs
+++ src/OrchardCoreContrib.ContentPreview/Startup.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
-using OrchardCore.Environment.Shell;
 using OrchardCore.Modules;
 using OrchardCore.Mvc.Core.Utilities;
 using OrchardCoreContrib.ContentPreview.Controllers;
@@ -17,17 +13,6 @@ namespace OrchardCoreContrib.ContentPreview
     /// </summary>
     public class Startup : StartupBase
     {
-        private readonly AdminOptions _adminOptions;
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="Startup"/>.
-        /// </summary>
-        /// <param name="adminOptions">The <see cref="IOptions<AdminOptions>"/>.</param>
-        public Startup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
-
         /// <inheritdoc/>
         public override void ConfigureServices(IServiceCollection services)
         {
@@ -37,39 +22,7 @@ namespace OrchardCoreContrib.ContentPreview
         /// <inheritdoc/>
         public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
         {
-            var shellFeaturesManager = serviceProvider.GetService<IShellFeaturesManager>();
-            var shellSettings = serviceProvider.GetService<ShellSettings>();
-
-            app.Use(async (context, next) =>
-            {
-                var path = context.Request.Path.Value;
-                if (path.StartsWith($"/{_adminOptions.AdminUrlPrefix}", StringComparison.OrdinalIgnoreCase))
-                {
-                    await next();
-
-                    return;
-                }
-
-                var featureEnabled = shellFeaturesManager
-                    .GetEnabledExtensionsAsync().Result
-                    .Any(f => f.Id == "OrchardCoreContrib.ContentPreview");
-
-                if (!featureEnabled)
-                {
-                    await next();
-
-                    return;
-                }
-
-                var isPreview = context.Request.Query.ContainsKey("preview");
-
-                if (!path.Contains("preview") && !isPreview)
-                {
-                    context.Response.Redirect($"/{shellSettings.RequestUrlPrefix}/preview{path}");
-                }
-
-                await next();
-            });
+            app.UsePagePreview();
 
             routes.MapAreaControllerRoute(
                 name: "PreviewContent",
