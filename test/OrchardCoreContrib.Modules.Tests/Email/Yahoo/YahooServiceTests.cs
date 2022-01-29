be744a5983b359dcc72f4af87eef394581b089bc
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-01-29 17:23:10 +0300

    Add unit test for OCC.Email.Yahoo

---
+++ test/OrchardCoreContrib.Modules.Tests/Email/Yahoo/YahooServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using OrchardCore.Email;
+using OrchardCoreContrib.Email.Yahoo;
+using OrchardCoreContrib.Email.Yahoo.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OrchardCoreContrib.Modules.Email.Yahoo.Tests
+{
+    public class YahooServiceTests
+    {
+        [Fact(Skip = "Set Yahoo user credentials and default sender before run this test.")]
+        public async Task Send()
+        {
+            // Arrange
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("8471ee66-1e1d-4acf-8ea8-5e03556514b0")
+                .Build();
+            var settings = new YahooSettings
+            {
+                DefaultSender = config["Email:From"],
+                UserName = config["Email:Yahoo:Username"],
+                Password = config["Email:Yahoo:Password"]
+            };
+            var logger = Mock.Of<ILogger<YahooService>>();
+            var localizer = Mock.Of<IStringLocalizer<YahooService>>();
+            var service = new YahooService(Options.Create(settings), logger, localizer);
+            var message = new MailMessage
+            {
+                To = config["Email:To"],
+                Subject = "Test",
+                Body = "Test message."
+            };
+
+            // Act
+            var result = await service.SendAsync(message);
+
+            // Assert
+            Assert.True(result.Succeeded);
+        }
+    }
+}
