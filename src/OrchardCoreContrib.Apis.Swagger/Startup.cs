b31eb41ddebf96d81ea3908e2eb8d017c21915d5
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-02-02 21:29:25 +0300

    Add Swagger module

---
+++ src/OrchardCoreContrib.Apis.Swagger/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+using OrchardCoreContrib.OpenApi.Abstractions;
+
+namespace OrchardCoreContrib.Apis.Swagger
+{
+    /// <summary>
+    /// Represensts a startup point to register the required services by Swagger module.
+    /// </summary>
+    public class Startup : StartupBase
+    {
+        /// <inheritdoc/>
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            var swaggerApiDefinition = new SwaggerApiDefinition();
+            
+            services.AddTransient<IOpenApiDefinition>(sp => swaggerApiDefinition);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(swaggerApiDefinition.Version, swaggerApiDefinition.Info);
+            });
+        }
+
+        /// <inheritdoc/>
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+            => builder.UseSwagger();
+    }
+}
