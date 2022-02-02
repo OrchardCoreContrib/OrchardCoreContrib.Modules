c7fc4adfff2fc26c4373236da415e5d66b07cc27
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-02-02 21:11:10 +0300

    Add OpenAPI abstractions

---
+++ src/OrchardCoreContrib.OpenApi.Abstractions/IOpenApiDefinition.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace OrchardCoreContrib.OpenApi.Abstractions
+{
+    /// <summary>
+    /// Represents a contract for defining OpenAPI object.
+    /// </summary>
+    public interface IOpenApiDefinition
+    {
+        /// <summary>
+        /// Gets the application name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the version OpenAPI document.
+        /// </summary>
+        string Version { get; }
+
+        /// <summary>
+        /// Gets the application description.
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// Gets a metadata for OpenAPI object.
+        /// </summary>
+        OpenApiInfo Info { get; }
+    }
+}
---
+++ src/OrchardCoreContrib.OpenApi.Abstractions/OpenApiDefinition.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace OrchardCoreContrib.OpenApi.Abstractions
+{
+    /// <summary>
+    /// Represents a base class for OpenAPI definition.
+    /// </summary>
+    public abstract class OpenApiDefinition : IOpenApiDefinition
+    {
+        /// <inheritdoc/>
+        public abstract string Name { get; }
+
+        /// <inheritdoc/>
+        public virtual string Description { get; }
+
+        /// <inheritdoc/>
+        public abstract string Version { get; }
+
+        /// <inheritdoc/>
+        public OpenApiInfo Info => new OpenApiInfo
+        {
+            Version = Version,
+            Title = Name,
+            Description = Description
+        };
+    }
+}
