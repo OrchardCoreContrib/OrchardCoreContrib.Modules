3b22bff5bb8a39e5e41c0fd9a418bff0a688b16a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-03-05 23:26:28 +0300

    Add DataResourceStringProviderExtensions

---
+++ src/OrchardCoreContrib.Localization.Abstractions/Data/Extensions/DataResourceStringProviderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using OrchardCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrchardCoreContrib.Localization.Data
+{
+    /// <summary>
+    /// Provides an extension methods for <see cref="IDataResourceStringProvider"/>.
+    /// </summary>
+    public static class DataResourceStringProviderExtensions
+    {
+        private const char RecordKeySeparator = '|';
+
+        /// <summary>
+        /// Gets the resource strings.
+        /// </summary>
+        /// <param name="resourceStringProvider">The <see cref="IDataResourceStringProvider"/>.</param>
+        /// <param name="context">The resource context.</param>
+        public static IEnumerable<CultureDictionaryRecordKey> GetAllResourceStrings(this IDataResourceStringProvider resourceStringProvider, string context)
+        {
+            if (resourceStringProvider is null)
+            {
+                throw new ArgumentNullException(nameof(resourceStringProvider));
+            }
+
+            if (string.IsNullOrEmpty(context))
+            {
+                throw new ArgumentException($"'{nameof(context)}' cannot be null or empty.", nameof(context));
+            }
+
+            return resourceStringProvider
+                .GetAllResourceStrings()
+                .Where(s => s.ToString().StartsWith(context.ToLower() + RecordKeySeparator));
+        }
+    }
+}
---
+++ test/OrchardCoreContrib.Tests/Localization/Data/Extensions/DataResourceStringProviderExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using OrchardCore.Localization;
+using System.Linq;
+using Xunit;
+
+namespace OrchardCoreContrib.Localization.Data.Tests
+{
+    public class DataResourceStringProviderExtensionsTests
+    {
+        [Theory]
+        [InlineData("Content Type", 1)]
+        [InlineData("Menu", 1)]
+        [InlineData("Content Field", 2)]
+        [InlineData("content field", 2)]
+        [InlineData("None", 0)]
+        public void GetResourceStrings(string context, int expectedResourcesNumber)
+        {
+            // Arrange
+            var resourceStringProvider = new TestResourceStringProvider();
+
+            // Act
+            var resourceStrings = resourceStringProvider.GetAllResourceStrings(context);
+
+            // Assert
+            Assert.Equal(expectedResourcesNumber, resourceStrings.Count());
+        }
+    }
+}
