7a7cff5b0e3f1c0cfabac06173a593b07841be07
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-03-06 12:14:38 +0300

    Prettify Users manifest

--- src/OrchardCoreContrib.Users/Manifest.cs
+++ src/OrchardCoreContrib.Users/Manifest.cs
@@ -2,17 +2,15 @@
 
 [assembly: Module(
     Name = "Users",
-    Author = "Orchard Core Contrib",
-    Website = "",
+    Author = "The Orchard Core Contrib Team",
     Version = "1.1.0",
     Category = "Security"
 )]
 
 [assembly: Feature(
     Id = "OrchardCoreContrib.Users.Impersonation",
-    Name = "Impersonation",
+    Name = "Users Impersonation",
     Description = "This feature allow administrators to sign in with other user identity.",
-    Dependencies = new[] { "OrchardCore.Users" },
-    Category = "Security"
+    Dependencies = new[] { "OrchardCore.Users" }
 )]
 
