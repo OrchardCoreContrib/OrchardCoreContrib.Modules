ed8e6371be7fc65be0cd46c4b19191f38d07e2a7
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-03-06 12:58:57 +0300

    Prettify emails manifests

--- src/OrchardCoreContrib.Email.Yahoo/Manifest.cs
+++ src/OrchardCoreContrib.Email.Yahoo/Manifest.cs
@@ -2,17 +2,8 @@
 
 [assembly: Module(
     Name = "Yahoo",
-    Author = "Orchard Core Contrib",
-    Website = "",
+    Author = "The Orchard Core Contrib Team",
     Version = "1.1.0",
-    Description = "Provides email settings configuration and a default email service based on Yahoo service.",
-    Dependencies = new string[] { "OrchardCore.Email" },
-    Category = "Messaging"
-)]
-
-[assembly: Feature(
-    Id = "OrchardCoreContrib.Email.Yahoo",
-    Name = "Yahoo",
-    Description = "Allow you to send email(s) via Yahoo service.",
+    Description = "Provides email settings configuration for Yahoo service.",
     Category = "Messaging"
 )]
