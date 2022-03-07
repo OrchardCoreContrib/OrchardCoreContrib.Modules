492d4d15bb7fcf495179f670c7c82e4e7911a350
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-03-07 21:14:34 +0300

    Avoid render the page preview bar inside the IFrame

---
+++ src/OrchardCoreContrib.ContentPreview/RequestDestination.cs
@@ -0,0 +1,9 @@
+﻿namespace OrchardCoreContrib.ContentPreview
+{
+    public class RequestDestination
+    {
+        public readonly static string Document = "document";
+
+        public readonly static string Iframe = "iframe";
+    }
+}
