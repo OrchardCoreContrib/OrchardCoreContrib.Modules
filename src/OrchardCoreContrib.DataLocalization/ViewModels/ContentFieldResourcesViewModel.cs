ffd3fc43eaed33f6d46cb13c600f21dd6d085c46
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-03-17 22:36:32 +0300

    Refactoring

--- src/OrchardCoreContrib.DataLocalization/ViewModels/ContentFieldResourcesViewModel.cs
+++ src/OrchardCoreContrib.DataLocalization/ViewModels/ContentFieldResourcesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using OrchardCoreContrib.DataLocalization.Models;
+using System;
 using System.Collections.Generic;
 
 namespace OrchardCoreContrib.DataLocalization.ViewModels
@@ -10,5 +11,11 @@ namespace OrchardCoreContrib.DataLocalization.ViewModels
         public IEnumerable<string> ResourcesNames { get; set; }
 
         public IEnumerable<Translation> Translations { get; set; }
+
+        public string SelectedContentType { get; set; }
+
+        public string SelectedCulture { get; set; }
+
+        public bool HasCulture => !String.IsNullOrEmpty(SelectedCulture);
     }
 }
