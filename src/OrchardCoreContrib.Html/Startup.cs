c0462ab78524fb1f60ac2028d7483cd218d4aad3
kudryavtsevda <diak3@yandex.ru>
2022-03-22 22:21:38 +0400

    add margin 0 add lost controllers and views

---
+++ src/OrchardCoreContrib.Html/Startup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using OrchardCore.Modules;
+using OrchardCore.Mvc.Core.Utilities;
+using OrchardCoreContrib.Html.Controllers;
+
+namespace OrchardCoreContrib.Html
+{
+    public class Startup : StartupBase
+    {
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "GrapesJSEditor",
+                areaName: "OrchardCoreContrib.Html",
+                pattern: "GrapesJSEditor",
+                defaults: new { controller = typeof(AdminController).ControllerName(), action = nameof(AdminController.Index) }
+            );
+        }
+    }
+}
