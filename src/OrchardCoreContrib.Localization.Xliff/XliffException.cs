7d3adb51fac9aa30a4698392df9f2e8471c7c6bc
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-09-13 12:17:49 +0300

    Add XLIFF localization

---
+++ src/OrchardCoreContrib.Localization.Xliff/XliffException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OrchardCoreContrib.Localization.Xliff;
+
+public class XliffException : Exception
+{
+    public XliffException()
+    {
+    }
+
+    public XliffException(string message) : base(message)
+    {
+    }
+
+    public XliffException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ src/OrchardCoreContrib.Localization.Xliff/XliffReader.cs
@@ -0,0 +1,43 @@
+﻿using OrchardCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace OrchardCoreContrib.Localization.Xliff;
+
+public class XliffReader
+{
+    public static async Task<IEnumerable<CultureDictionaryRecord>> ParseAsync(Stream stream)
+    {
+        if (stream is null)
+        {
+            throw new ArgumentNullException(nameof(stream));
+        }
+
+        try
+        {
+            var doc = await XDocument.LoadAsync(stream, LoadOptions.None, CancellationToken.None);
+
+            var cultureRecords = new List<CultureDictionaryRecord>();
+            var @namespace = doc.Root.Name.Namespace;
+            foreach (var unitElement in doc.Descendants(@namespace + "unit"))
+            {
+                var segmentElement = unitElement.Element(@namespace + "segment");
+                var key = segmentElement.Element(@namespace + "source").Value;
+                var value = segmentElement.Element(@namespace + "target").Value;
+
+                cultureRecords.Add(new CultureDictionaryRecordWrapper(key, value));
+            }
+
+            return cultureRecords;
+        }
+        catch (Exception ex)
+        {
+            throw new XliffException("Invalid XLIFF document.", ex);
+        }
+    }
+}
