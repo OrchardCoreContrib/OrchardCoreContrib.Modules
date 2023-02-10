5d76084bb71da41208673bd54417b9acf0645d74
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-11 00:36:49 +0300

    Add AvatarOptions

---
+++ src/OrchardCoreContrib.Users/AvatarOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace OrchardCoreContrib.Users;
+
+public class AvatarOptions
+{
+    public string BackColor { get; set; } = "#55B670";
+
+    public string ForeColor { get; set; } = "#FFF";
+}
--- src/OrchardCoreContrib.Users/Services/AvatarService.cs
+++ src/OrchardCoreContrib.Users/Services/AvatarService.cs
@@ -1,4 +1,5 @@
-﻿using SixLabors.Fonts;
+﻿using Microsoft.Extensions.Options;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.Drawing;
@@ -13,17 +14,27 @@ namespace OrchardCoreContrib.Users.Services;
 
 public class AvatarService : IAvatarService
 {
+    private readonly AvatarOptions _avatarOptions;
+
+    public AvatarService(IOptions<AvatarOptions> avatarOptions)
+    {
+        _avatarOptions = avatarOptions.Value;
+    }
+
     public string Generate(string userName)
     {
         var width = 30;
         var height = 30;
         using (var image = new Image<Rgba32>(width, height))
         {
+            _ = Color.TryParseHex(_avatarOptions.BackColor, out var backColor);
+            _ = Color.TryParseHex(_avatarOptions.ForeColor, out var foreColor);
+
             var yourPolygon = new EllipsePolygon(x: width / 2, y: height / 2, width: width, height: height);
-            image.Mutate(x => x.Fill(Color.FromRgb(85, 182, 112), yourPolygon));
+            image.Mutate(x => x.Fill(backColor, yourPolygon));
 
             var font = SystemFonts.CreateFont("Segoe UI", 16, FontStyle.Regular);
-            image.Mutate(x => x.DrawText(userName[0].ToString().ToUpper(), font, Color.White, new PointF(width / 3, height / 6)));
+            image.Mutate(x => x.DrawText(userName[0].ToString().ToUpper(), font, foreColor, new PointF(width / 3, height / 6)));
 
             var imreBase64Data = ToBase64String(image);
 
