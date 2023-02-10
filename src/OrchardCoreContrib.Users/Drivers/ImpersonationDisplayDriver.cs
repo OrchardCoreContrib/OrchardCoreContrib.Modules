342b46d4328d0d975f8e846b7ec3d0f7bc807f29
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-10 23:51:37 +0300

    Remove UserButtons

---
+++ src/OrchardCoreContrib.Users/Drivers/ImpersonationDisplayDriver.cs
@@ -0,0 +1,15 @@
+﻿using OrchardCore.DisplayManagement.Handlers;
+using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Modules;
+using OrchardCore.Users.Models;
+using OrchardCore.Users.ViewModels;
+
+namespace OrchardCoreContrib.Users.Drivers;
+
+[Feature("OrchardCoreContrib.Users.Impersonation")]
+public class ImpersonationDisplayDriver : DisplayDriver<User>
+{
+    public override IDisplayResult Display(User user)
+        => Initialize<SummaryAdminUserViewModel>("ImpersonationButton", model => model.User = user)
+            .Location("SummaryAdmin", "Actions:2");
+}
