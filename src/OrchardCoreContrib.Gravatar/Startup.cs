e4d29cfa223093ecf5c7d3bc7e09463e7ae27521
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-11 19:13:40 +0300

    Add Gravatar module

---
+++ src/OrchardCoreContrib.Gravatar/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+using OrchardCoreContrib.Gravatar.TagHelpers;
+
+namespace OrchardCoreContrib.Gravatar;
+
+/// <summary>
+/// Represents an entry point to register the user avatar required services.
+/// </summary>
+public class Startup : StartupBase
+{
+    /// <inheritdoc/>
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTagHelpers<GravatarTagHelper>();
+    }
+}
