a73d8bc671ada5d56d4b31317c4bb3b37a99278d
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-21 00:50:23 +0300

    Add TransliterationServiceCollectionExtensions

---
+++ src/OrchardCoreContrib.ContentLocalization.Core/Extensions/TransliterationServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using OrchardCoreContrib.ContentLocalization.Transliteration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class TransliterationServiceCollectionExtensions
+{
+    public static IServiceCollection AddTransliteration(this IServiceCollection services)
+    {
+        services.AddSingleton<ITransliterateRuleProvider, DefaultTransliterateRuleProvider>();
+
+        return services;
+    }
+}
