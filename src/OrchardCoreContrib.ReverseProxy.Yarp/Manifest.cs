05b6c5943ce5f9065737050dacb570c36f788757
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-26 17:24:15 +0300

    Add OCC.ReverseProxy.Yarp

---
+++ src/OrchardCoreContrib.ReverseProxy.Yarp/Manifest.cs
@@ -0,0 +1,11 @@
+﻿using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Yet Another Reverse Proxy (YARP)",
+    Author = "The Orchard Core Contrib Team",
+    Version = "1.0.0",
+    Description = "Enables configuration of hosting scenarios with a reverse proxy using YARP",
+    Category = "Infrastructure"
+)]
+
+
---
+++ src/OrchardCoreContrib.ReverseProxy.Yarp/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell.Configuration;
+using OrchardCore.Modules;
+using System;
+
+namespace OrchardCoreContrib.ReverseProxy.Yarp;
+
+public class Startup : StartupBase
+{
+    private readonly IShellConfiguration _shellConfiguration;
+
+    public Startup(IShellConfiguration shellConfiguration)
+    {
+        _shellConfiguration = shellConfiguration;
+    }
+
+    public override int Order => -1;
+
+    public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        routes.MapReverseProxy();
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddReverseProxy()
+            .LoadFromConfig(_shellConfiguration.GetSection("OrchardCoreContrib_Yarp"));
+    }
+}
