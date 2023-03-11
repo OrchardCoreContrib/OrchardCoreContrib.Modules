8083b6f4ebf6173b5480e59c096bc8001b751c7a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-11 16:14:49 +0300

    Add OCC.HealthChecks module

---
+++ src/OrchardCoreContrib.HealthChecks/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Health Checks",
+    Author = "The Orchard Core Contrib Team",
+    Version = "1.0.0",
+    Description = "Provides health checks for the website.",
+    Category = "Infrastructure"
+)]
---
+++ src/OrchardCoreContrib.HealthChecks/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OrchardCore.Environment.Shell.Configuration;
+using OrchardCore.Modules;
+using OrchardCoreContrib.HealthChecks.Models;
+using System.Net.Mime;
+
+namespace OrchardCoreContrib.HealthChecks;
+public class Startup : StartupBase
+{
+    private readonly IShellConfiguration _shellConfiguration;
+
+    public Startup(IShellConfiguration shellConfiguration)
+    {
+        _shellConfiguration = shellConfiguration;
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHealthChecks();
+
+        services.Configure<HealthChecksOptions>(_shellConfiguration.GetSection("OrchardCoreContrib_HealthChecks"));
+    }
+
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        var healthChecksOptions = serviceProvider.GetService<IOptions<HealthChecksOptions>>().Value;
+
+        if (healthChecksOptions.ShowDetails)
+        {
+            routes.MapHealthChecks(healthChecksOptions.Url, new HealthCheckOptions
+            {
+                AllowCachingResponses = false,
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                },
+                ResponseWriter = WriteResponse
+            });
+        }
+        else
+        {
+            routes.MapHealthChecks(healthChecksOptions.Url);
+        }
+    }
+
+    private static async Task WriteResponse(HttpContext context, HealthReport report)
+    {
+        var response = new HealthCheckReponse
+        {
+            Status = report.Status.ToString(),
+            Duration = report.TotalDuration,
+            HealthChecks = report.Entries.Select(item => new HealthCheckEntry
+            {
+                Name = item.Key,
+                Status = item.Value.Status.ToString(),
+                Description = item.Value.Description
+            })
+        };
+
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(response, Formatting.Indented));
+    }
+}
