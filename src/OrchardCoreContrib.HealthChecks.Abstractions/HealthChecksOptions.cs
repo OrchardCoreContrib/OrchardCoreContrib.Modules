09ffafcc0bd5c7e03735e139e8f2bf854065b02b
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-11 15:30:46 +0300

    Add OCC.HealthChecks.Abstractions

---
+++ src/OrchardCoreContrib.HealthChecks.Abstractions/HealthChecksOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace OrchardCoreContrib.HealthChecks;
+
+/// <summary>
+/// Represents a programmable health chekcs options.
+/// </summary>
+public class HealthChecksOptions
+{
+    /// <summary>
+    /// Gets or sets the health check URL. Default to "/health.
+    /// </summary>
+    public string Url { get; set; } = "/health";
+
+    /// <summary>
+    /// Whether to show the details for the checks dependency or not. Default to <c>false</c>.
+    /// </summary>
+    public bool ShowDetails { get; set; }
+}
