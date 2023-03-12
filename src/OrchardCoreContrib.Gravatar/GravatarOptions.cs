b23a2b756e009045291e87b02ff1496cc78630b1
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-12 17:25:25 +0300

    Add Gravatar liquid filter

---
+++ src/OrchardCoreContrib.Gravatar/GravatarOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrchardCoreContrib.Gravatar;
+
+public class GravatarOptions
+{
+    public string DefaultImage { get; set; }
+
+    public GravatarRating Rating { get; set; } = GravatarRating.PG;
+
+    [Range(1, 512)]
+    public int Size { get; set; } = 24;
+}
--- src/OrchardCoreContrib.Gravatar/Startup.cs
+++ src/OrchardCoreContrib.Gravatar/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell.Configuration;
+using OrchardCore.Liquid;
 using OrchardCore.Modules;
+using OrchardCoreContrib.Gravatar.Liquid;
+using OrchardCoreContrib.Gravatar.Services;
 using OrchardCoreContrib.Gravatar.TagHelpers;
 
 namespace OrchardCoreContrib.Gravatar;
@@ -9,9 +13,22 @@ namespace OrchardCoreContrib.Gravatar;
 /// </summary>
 public class Startup : StartupBase
 {
+    private readonly IShellConfiguration _shellConfiguration;
+
+    public Startup(IShellConfiguration shellConfiguration)
+    {
+        _shellConfiguration = shellConfiguration;
+    }
+
     /// <inheritdoc/>
     public override void ConfigureServices(IServiceCollection services)
     {
+        services.AddScoped<IGravatarService, GravatarService>();
+
         services.AddTagHelpers<GravatarTagHelper>();
+
+        services.AddLiquidFilter<GravatarFilter>("to_gravatar");
+
+        services.Configure<GravatarOptions>(_shellConfiguration.GetSection("OrchardCoreContrib_Gravatar"));
     }
 }
