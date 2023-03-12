821bc2bb167536deff9978b4239c6a838f3447e3
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-12 11:10:09 +0300

    Add SendGrid health check

---
+++ src/OrchardCoreContrib.Email.SendGrid/HealthChecks/SendGridHealthCheck.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
+using System.Net.Http;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace OrchardCoreContrib.Email.SendGrid.HealthChecks;
+
+public class SendGridHealthCheck : IHealthCheck
+{
+    internal const string Name = "SendGrid Health Check";
+
+    private readonly SendGridSettings _sendGridSettings;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public SendGridHealthCheck(IOptions<SendGridSettings> sendGridSettings, IHttpClientFactory httpClientFactory)
+    {
+        _sendGridSettings = sendGridSettings.Value;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var httpClient = _httpClientFactory.CreateClient("SendGrid");
+
+            var client = new SendGridClient(httpClient, _sendGridSettings.ApiKey);
+            var message = CreateMessage();
+
+            var response = await client.SendEmailAsync(message, cancellationToken).ConfigureAwait(false);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return HealthCheckResult.Healthy();
+            }
+            else
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, "Sending an email to SendGrid using the sandbox mode is not responding.");
+            }
+        }
+        catch (Exception ex)
+        {
+            return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+        }
+    }
+
+    private SendGridMessage CreateMessage()
+    {
+        var from = new EmailAddress(_sendGridSettings.DefaultSender);
+        var to = new EmailAddress(_sendGridSettings.DefaultSender);
+        
+        var message = MailHelper
+            .CreateSingleEmail(from, to, "SendGrid Health Check", "This is a test email to check if the SendGrid service is healthy.", null);
+
+        message.SetSandBoxMode(true);
+
+        return message;
+    }
+}
---
+++ src/OrchardCoreContrib.Email.SendGrid/HealthChecks/Startup.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Modules;
+using OrchardCoreContrib.HealthChecks;
+using System;
+
+namespace OrchardCoreContrib.Email.SendGrid.HealthChecks;
+
+[RequireFeatures("OrchardCoreContrib.HealthChecks")]
+public class Startup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHttpClient();
+
+        services.AddHealthChecks()
+            .AddSendGridCheck();
+    }
+
+    public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        var healthChecksOptions = serviceProvider.GetService<IOptions<HealthChecksOptions>>().Value;
+
+        routes.MapHealthChecks($"{healthChecksOptions.Url}/sendgrid", new HealthCheckOptions
+        {
+            Predicate = r => r.Name == SendGridHealthCheck.Name
+        });
+    }
+}
