9a6b62dae7d657273148d692af8cc0ff696ccbf0
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-12 11:10:09 +0300

    Add SendGrid health check

---
+++ src/OrchardCoreContrib.Email.SendGrid/HealthChecks/Extensions/SendGridHealthCheckExtensions.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCoreContrib.Email.SendGrid.HealthChecks;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class SendGridHealthCheckExtensions
+{
+    public static IHealthChecksBuilder AddSendGridCheck(this IHealthChecksBuilder healthChecksBuilder)
+        => healthChecksBuilder.AddCheck<SendGridHealthCheck>(SendGridHealthCheck.Name);
+}
--- src/OrchardCoreContrib.Email.SendGrid/SendGridSettings.cs
+++ src/OrchardCoreContrib.Email.SendGrid/SendGridSettings.cs
@@ -1,12 +1,20 @@
 ﻿using OrchardCore.Email;
+using EmailAddress = OrchardCore.Email.EmailAddressAttribute;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrchardCoreContrib.Email.SendGrid
 {
     /// <summary>
     /// Represents a settings for SendGrid.
     /// </summary>
-    public class SendGridSettings : SmtpSettings
+    public class SendGridSettings
     {
+        /// <summary>
+        /// Gets or sets the default sender mail.
+        /// </summary>
+        [Required(AllowEmptyStrings = false), EmailAddress]
+        public string DefaultSender { get; set; }
+
         /// <summary>
         /// Gets or sets the API key.
         /// </summary>
