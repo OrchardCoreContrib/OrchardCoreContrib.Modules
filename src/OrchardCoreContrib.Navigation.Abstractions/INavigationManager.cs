176a90f1a313cd0fe6099bea7cd9cc75e7ad836f
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-04-14 21:17:13 +0300

    Remove unnecessary Navigation APIs

--- src/OrchardCoreContrib.Navigation.Abstractions/INavigationManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace OrchardCoreContrib.Navigation;
-
-public interface INavigationManager
-{
-    Task<IEnumerable<MenuItem>> BuildMenuAsync(string name, ActionContext context);
-}
