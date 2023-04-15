64cc9f623a3b5a6c8da18c7d14c724439169b021
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-04-16 01:09:39 +0300

    Lazy load Migration in MigrationDictionaryRecord

--- src/OrchardCoreContrib.Data.Abstractions/Migrations/MigrationDictionaryRecord.cs
+++ src/OrchardCoreContrib.Data.Abstractions/Migrations/MigrationDictionaryRecord.cs
@@ -2,13 +2,15 @@
 
 public class MigrationDictionaryRecord
 {
+    private readonly Lazy<IMigration> _migration;
+
     public MigrationDictionaryRecord(long id, IMigration migration)
     {
         Id = id;
-        Migration = migration;
+        _migration = new Lazy<IMigration>(migration);
     }
 
     public long Id { get; }
 
-    public IMigration Migration { get; }
+    public IMigration Migration => _migration.Value;
 }
