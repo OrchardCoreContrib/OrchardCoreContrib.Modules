65a053fa9931a3ae7862c52176d67e94f4dd9b91
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-09-22 17:28:49 +0300

    Bump OCC.Users to 1.4.0

--- src/OrchardCoreContrib.Users/Manifest.cs
+++ src/OrchardCoreContrib.Users/Manifest.cs
@@ -5,7 +5,7 @@ using ManifestConstants = OrchardCoreContrib.Modules.Manifest.ManifestConstants;
     Name = "Users",
     Author = ManifestConstants.Author,
     Website = ManifestConstants.Website,
-    Version = "1.3.0",
+    Version = "1.4.0",
     Category = "Security"
 )]
 
