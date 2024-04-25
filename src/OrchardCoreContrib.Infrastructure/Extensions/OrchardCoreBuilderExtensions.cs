93ebc4104a4da8e2699d20fcadee1ad7e7c72e49
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-04-25 12:52:14 +0300

    Add AddPhoneValidator() extension method

---
+++ src/OrchardCoreContrib.Infrastructure/Extensions/OrchardCoreBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using OrchardCoreContrib.Validation;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Provides an extension method for <see cref="OrchardCoreBuilder"/>.
+/// </summary>
+public static class OrchardCoreBuilderExtensions
+{
+    /// <summary>
+    /// Adds phone number validator service.
+    /// </summary>
+    /// <param name="builder">The <see cref="OrchardCoreBuilder"/>.</param>
+    public static OrchardCoreBuilder AddPhoneNumberValidator(this OrchardCoreBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddTransient<IPhoneNumberValidator, PhoneNumberValidator>();
+        });
+
+        return builder;
+    }
+}
