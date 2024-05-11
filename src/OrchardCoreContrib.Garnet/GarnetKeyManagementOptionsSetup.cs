e1b6dfd1afc0c3a1ccedb3271a0634eb10cd95cb
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-05-12 01:33:07 +0300

    Add Garnet data protection feature

---
+++ src/OrchardCoreContrib.Garnet/GarnetKeyManagementOptionsSetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Options;
+using OrchardCore.Environment.Shell;
+using OrchardCoreContrib.Garnet.Services;
+
+namespace OrchardCoreContrib.Garnet;
+
+/// <summary>
+/// Represents a setup to configure the <see cref="KeyManagementOptions"/> for Garnet data protection feature.
+/// </summary>
+/// <param name="garnetService">The <see cref="IGarnetService"/>.</param>
+/// <param name="shellSettings">The <see cref="ShellSettings"/>.</param>
+public class GarnetKeyManagementOptionsSetup(IGarnetService garnetService, ShellSettings shellSettings)
+    : IConfigureOptions<KeyManagementOptions>
+{
+    private readonly string _tenant = shellSettings.Name;
+
+    /// <inheritdoc/>
+    public void Configure(KeyManagementOptions options)
+    {
+        options.XmlRepository = new GarnetXmlRepository(() =>
+        {
+            if (garnetService.Client == null)
+            {
+                garnetService.ConnectAsync().GetAwaiter().GetResult();
+            }
+
+            return garnetService.Client;
+        }, $"({garnetService.InstancePrefix}{_tenant}:DataProtection-Keys");
+    }
+}
---
+++ src/OrchardCoreContrib.Garnet/Startups/GarnetDataProtectionStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Modules;
+using OrchardCoreContrib.Garnet.Services;
+
+namespace OrchardCoreContrib.Garnet;
+
+/// <summary>
+/// Represensts a startup point to register the required services by Garnet data protection feature.
+/// </summary>
+[Feature("OrchardCoreContrib.Garnet.DataProtection")]
+public class GarnetDataProtectionStartup : StartupBase
+{
+    /// <inheritdoc/>
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        if (services.Any(d => d.ServiceType == typeof(IGarnetService)))
+        {
+            services.AddTransient<IConfigureOptions<KeyManagementOptions>, GarnetKeyManagementOptionsSetup>();
+        }
+    }
+}
