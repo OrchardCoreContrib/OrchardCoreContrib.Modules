d8b5ba614ae46420e49d08c4ab97ae83bf873eb7
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-11-22 16:09:10 +0300

    Apply suggestions from code review

--- src/OrchardCoreContrib.ContentLocalization/Liquid/ArabToLatFilter.cs
+++ src/OrchardCoreContrib.ContentLocalization/Liquid/ArabToLatFilter.cs
@@ -12,6 +12,7 @@ public class ArabToLatFilter(ITransliterationService transliterationService) : I
     public ValueTask<FluidValue> ProcessAsync(FluidValue input, FilterArguments arguments, LiquidTemplateContext context)
     {
         var text = input.ToStringValue();
+
         return new StringValue(transliterationService.Transliterate(TransliterateScript.Arabic, text));
     }
 }
