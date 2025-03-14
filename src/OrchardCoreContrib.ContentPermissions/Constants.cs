d6ae109e53490f2773149ba080bd452633681e29
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-03-14 14:27:20 +0300

    Add docs

--- src/OrchardCoreContrib.ContentPermissions/Constants.cs
+++ src/OrchardCoreContrib.ContentPermissions/Constants.cs
@@ -1,6 +1,12 @@
 ﻿namespace OrchardCoreContrib.ContentPermissions;
 
+/// <summary>
+/// Represents a class that contains constants.
+/// </summary>
 public sealed class Constants
 {
+    /// <summary>
+    /// Represents the name of the content permissions part.
+    /// </summary>
     public const string ContentPermissionsPartName = "ContentPermissionsPart";
 }
