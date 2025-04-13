801a8d211c3524c3ea5ecf2561440bf9c6c466c8
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-04-13 10:52:51 +0300

    Add UserByUserGroupNameIndex

---
+++ src/OrchardCoreContrib.UserGroups/Indexes/UserByGroupNameIndex.cs
@@ -0,0 +1,10 @@
+﻿using YesSql.Indexes;
+
+namespace OrchardCoreContrib.UserGroups.Indexes;
+
+public class UserByGroupNameIndex : ReduceIndex
+{
+    public string GroupName { get; set; }
+
+    public int Count { get; set; }
+}
