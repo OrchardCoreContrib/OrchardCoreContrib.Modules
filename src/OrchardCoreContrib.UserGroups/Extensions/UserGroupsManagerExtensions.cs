14610e230566cf4624ff02b030eb5eb37c7f3f53
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-04-13 13:51:19 +0300

    Add docs

--- src/OrchardCoreContrib.UserGroups/Extensions/UserGroupsManagerExtensions.cs
+++ src/OrchardCoreContrib.UserGroups/Extensions/UserGroupsManagerExtensions.cs
@@ -4,8 +4,15 @@ using OrchardCoreContrib.UserGroups.Models;
 
 namespace OrchardCoreContrib.UserGroups.Services;
 
+/// <summary>
+/// Provides extension methods for the <see cref="UserGroupsManager"/>.
+/// </summary>
 public static class UserGroupsManagerExtensions
 {
+    /// <summary>
+    /// Gets the user group names.
+    /// </summary>
+    /// <param name="userGroupsManager">The <see cref="UserGroupsManager"/>.</param>
     public static async Task<IEnumerable<string>> GetUserGroupNamesAsync(this UserGroupsManager userGroupsManager)
     {
         Guard.ArgumentNotNull(userGroupsManager, nameof(userGroupsManager));
@@ -15,6 +22,13 @@ public static class UserGroupsManagerExtensions
         return userGroups.Select(group => group.Name);
     }
 
+    /// <summary>
+    /// Creates a new user group with the specified name and description.
+    /// </summary>
+    /// <param name="userGroupsManager">The <see cref="UserGroupsManager"/>.</param>
+    /// <param name="name">The group name.</param>
+    /// <param name="description">The group description.</param>
+    /// <returns></returns>
     public static async Task<IdentityResult> CreateAsync(this UserGroupsManager userGroupsManager, string name, string description)
     {
         Guard.ArgumentNotNull(userGroupsManager, nameof(userGroupsManager));
