3cb749dda6dec9c18c0827a0fd90cf69a00acee6
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-11-28 18:32:54 +0300

    Use file-scope namespace

--- src/OrchardCoreContrib.Apis.Swagger/Startup.cs
+++ src/OrchardCoreContrib.Apis.Swagger/Startup.cs
@@ -3,30 +3,26 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 using OrchardCoreContrib.OpenApi.Abstractions;
-using System;
 
-namespace OrchardCoreContrib.Apis.Swagger
+namespace OrchardCoreContrib.Apis.Swagger;
+
+/// <summary>
+/// Represensts a startup point to register the required services by Swagger module.
+/// </summary>
+public class Startup : StartupBase
 {
-    /// <summary>
-    /// Represensts a startup point to register the required services by Swagger module.
-    /// </summary>
-    public class Startup : StartupBase
+    /// <inheritdoc/>
+    public override void ConfigureServices(IServiceCollection services)
     {
-        /// <inheritdoc/>
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            var swaggerApiDefinition = new SwaggerApiDefinition();
+        var swaggerApiDefinition = new SwaggerApiDefinition();
 
-            services.AddTransient<IOpenApiDefinition>(sp => swaggerApiDefinition);
+        services.AddTransient<IOpenApiDefinition>(sp => swaggerApiDefinition);
 
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc(swaggerApiDefinition.Version, swaggerApiDefinition.Info);
-            });
-        }
-
-        /// <inheritdoc/>
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-            => builder.UseSwagger();
+        services.AddSwaggerGen(options =>
+            options.SwaggerDoc(swaggerApiDefinition.Version, swaggerApiDefinition.Info));
     }
+
+    /// <inheritdoc/>
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        => builder.UseSwagger();
 }
--- src/OrchardCoreContrib.Apis.Swagger/SwaggerApiDefinition.cs
+++ src/OrchardCoreContrib.Apis.Swagger/SwaggerApiDefinition.cs
@@ -1,16 +1,15 @@
 ﻿using OrchardCoreContrib.OpenApi.Abstractions;
 
-namespace OrchardCoreContrib.Apis.Swagger
-{
-    /// <summary>
-    /// Respresents a swagger API definition for Orchard Core.
-    /// </summary>
-    public class SwaggerApiDefinition : OpenApiDefinition
-    {
-        /// <inheritdoc/>
-        public override string Name => "Orchard Core APIs Docs";
+namespace OrchardCoreContrib.Apis.Swagger;
 
-        /// <inheritdoc/>
-        public override string Version => "v1.0.0";
-    }
+/// <summary>
+/// Respresents a swagger API definition for Orchard Core.
+/// </summary>
+public class SwaggerApiDefinition : OpenApiDefinition
+{
+    /// <inheritdoc/>
+    public override string Name => "Orchard Core APIs Docs";
+
+    /// <inheritdoc/>
+    public override string Version => "v1.0.0";
 }
