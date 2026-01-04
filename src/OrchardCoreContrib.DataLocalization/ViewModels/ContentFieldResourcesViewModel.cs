610d8f09de57b8ff2b20be7a5df754e9b61fca93
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-04 22:08:54 +0300

    Bump OCC.DataLocalization to 1.3.0

--- src/OrchardCoreContrib.DataLocalization/ViewModels/ContentFieldResourcesViewModel.cs
+++ src/OrchardCoreContrib.DataLocalization/ViewModels/ContentFieldResourcesViewModel.cs
@@ -1,21 +1,18 @@
 ﻿using OrchardCoreContrib.DataLocalization.Models;
-using System;
-using System.Collections.Generic;
 
-namespace OrchardCoreContrib.DataLocalization.ViewModels
+namespace OrchardCoreContrib.DataLocalization.ViewModels;
+
+public class ContentFieldResourcesViewModel
 {
-    public class ContentFieldResourcesViewModel
-    {
-        public IEnumerable<string> ContentTypes { get; set; }
+    public IEnumerable<string> ContentTypes { get; set; }
 
-        public IEnumerable<string> ResourcesNames { get; set; }
+    public IEnumerable<string> ResourcesNames { get; set; }
 
-        public IEnumerable<Translation> Translations { get; set; }
+    public IEnumerable<Translation> Translations { get; set; }
 
-        public string SelectedContentType { get; set; }
+    public string SelectedContentType { get; set; }
 
-        public string SelectedCulture { get; set; }
+    public string SelectedCulture { get; set; }
 
-        public bool HasCulture => !String.IsNullOrEmpty(SelectedCulture);
-    }
+    public bool HasCulture => !string.IsNullOrEmpty(SelectedCulture);
 }
