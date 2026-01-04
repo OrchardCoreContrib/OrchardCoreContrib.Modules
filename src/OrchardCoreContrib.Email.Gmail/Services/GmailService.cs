cd6ebcf9fb1a8b6a839e9218259fc01810fb3a75
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-04 07:47:04 +0300

    Bump OCC.Email.Gmail to 1.6.0

--- src/OrchardCoreContrib.Email.Gmail/Services/GmailService.cs
+++ src/OrchardCoreContrib.Email.Gmail/Services/GmailService.cs
@@ -5,51 +5,43 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MimeKit;
 using OrchardCoreContrib.Email.Services;
-using System.Threading.Tasks;
 
-namespace OrchardCoreContrib.Email.Gmail.Services
+namespace OrchardCoreContrib.Email.Gmail.Services;
+
+/// <summary>
+/// Represents a service for sending emails using Gmail emailing service.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of <see cref="GmailService"/>.
+/// </remarks>
+/// <param name="gmailSetting">The <see cref="IOptions<GmailSettings>"/>.</param>
+/// <param name="logger">The <see cref="ILogger<GmailService>"/>.</param>
+/// <param name="stringLocalizer">The <see cref="IStringLocalizer<GmailService>"/>.</param>
+public class GmailService(
+    IOptions<GmailSettings> gmailSetting,
+    ILogger<GmailService> logger,
+    IStringLocalizer<GmailService> stringLocalizer) : SmtpService(gmailSetting, logger, stringLocalizer)
 {
-    /// <summary>
-    /// Represents a service for sending emails using Gmail emailing service.
-    /// </summary>
-    public class GmailService : SmtpService
+    private readonly GmailSettings _gmailSetting = gmailSetting.Value;
+
+    /// <inheritdoc/>
+    protected async override Task SendOnlineMessageAsync(MimeMessage message)
     {
-        private readonly GmailSettings _gmailSetting;
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="GmailService"/>.
-        /// </summary>
-        /// <param name="gmailSetting">The <see cref="IOptions<GmailSettings>"/>.</param>
-        /// <param name="logger">The <see cref="ILogger<GmailService>"/>.</param>
-        /// <param name="stringLocalizer">The <see cref="IStringLocalizer<GmailService>"/>.</param>
-        public GmailService(
-            IOptions<GmailSettings> gmailSetting,
-            ILogger<GmailService> logger,
-            IStringLocalizer<GmailService> stringLocalizer
-            ) : base(gmailSetting, logger, stringLocalizer)
+        using (var client = new SmtpClient())
         {
-            _gmailSetting = gmailSetting.Value;
-        }
+            client.ServerCertificateValidationCallback = CertificateValidationCallback;
+            
+            await client.ConnectAsync(_gmailSetting.Host, _gmailSetting.Port, SecureSocketOptions.StartTls);
 
-        /// <inheritdoc/>
-        protected async override Task SendOnlineMessageAsync(MimeMessage message)
-        {
-            using (var client = new SmtpClient())
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+            if (!string.IsNullOrWhiteSpace(_gmailSetting.UserName))
             {
-                client.ServerCertificateValidationCallback = CertificateValidationCallback;
-                
-                await client.ConnectAsync(_gmailSetting.Host, _gmailSetting.Port, SecureSocketOptions.StartTls);
-
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-                if (!string.IsNullOrWhiteSpace(_gmailSetting.UserName))
-                {
-                    await client.AuthenticateAsync(_gmailSetting.UserName, _gmailSetting.Password);
-                }
-
-                await client.SendAsync(message);
-                await client.DisconnectAsync(true);
+                await client.AuthenticateAsync(_gmailSetting.UserName, _gmailSetting.Password);
             }
+
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
         }
     }
 }
