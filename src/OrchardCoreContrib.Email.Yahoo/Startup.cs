b702c6213ca83f11628efc7978bb4fd85073f30d
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-04 08:29:16 +0300

    Bump OCC.Email.Yahoo 1.6.0

--- src/OrchardCoreContrib.Email.Yahoo/Startup.cs
+++ src/OrchardCoreContrib.Email.Yahoo/Startup.cs
@@ -13,46 +13,39 @@ using OrchardCore.Settings;
 using OrchardCoreContrib.Email.Yahoo.Controllers;
 using OrchardCoreContrib.Email.Yahoo.Drivers;
 using OrchardCoreContrib.Email.Yahoo.Services;
-using System;
 
-namespace OrchardCoreContrib.Email.Yahoo
+namespace OrchardCoreContrib.Email.Yahoo;
+
+/// <summary>
+/// Represensts a startup point to register the required services by Yahoo mailing module.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of <see cref="Startup"/>.
+/// </remarks>
+/// <param name="adminOptions">The <see cref="IOptions{AdminOptions}>.</param>
+public class Startup(IOptions<AdminOptions> adminOptions) : StartupBase
 {
-    /// <summary>
-    /// Represensts a startup point to register the required services by Yahoo mailing module.
-    /// </summary>
-    public class Startup : StartupBase
+    private readonly AdminOptions _adminOptions = adminOptions.Value;
+
+    /// <inheritdoc/>
+    public override void ConfigureServices(IServiceCollection services)
     {
-        private readonly AdminOptions _adminOptions;
+        services.AddScoped<IPermissionProvider, Permissions>();
+        services.AddScoped<IDisplayDriver<ISite>, YahooSettingsDisplayDriver>();
+        services.AddScoped<INavigationProvider, AdminMenu>();
 
-        /// <summary>
-        /// Initializes a new instance of <see cref="Startup"/>.
-        /// </summary>
-        /// <param name="adminOptions">The <see cref="IOptions{AdminOptions}>.</param>
-        public Startup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
+        services.AddTransient<IConfigureOptions<YahooSettings>, YahooSettingsConfiguration>();
+        services.AddScoped<ISmtpService, YahooService>();
+    }
 
-        /// <inheritdoc/>
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IPermissionProvider, Permissions>();
-            services.AddScoped<IDisplayDriver<ISite>, YahooSettingsDisplayDriver>();
-            services.AddScoped<INavigationProvider, AdminMenu>();
-
-            services.AddTransient<IConfigureOptions<YahooSettings>, YahooSettingsConfiguration>();
-            services.AddScoped<ISmtpService, YahooService>();
-        }
-
-        /// <inheritdoc/>
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            routes.MapAreaControllerRoute(
-                name: "YahooIndex",
-                areaName: "OrchardCoreContrib.Email.Yahoo",
-                pattern: _adminOptions.AdminUrlPrefix + "/Yahoo/Index",
-                defaults: new { controller = typeof(AdminController).ControllerName(), action = nameof(AdminController.Index) }
-            );
-        }
+    /// <inheritdoc/>
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        routes.MapAreaControllerRoute(
+            name: "YahooIndex",
+            areaName: "OrchardCoreContrib.Email.Yahoo",
+            pattern: _adminOptions.AdminUrlPrefix + "/Yahoo/Index",
+            defaults: new { controller = typeof(AdminController).ControllerName(), action = nameof(AdminController.Index) }
+        );
     }
 }
