eea9775cee29bb0a99248beb8dba483ff8ef23b8
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-04 08:04:06 +0300

    Bump OCC.Email.Hotmail to 1.6.0

--- src/OrchardCoreContrib.Email.Hotmail/Drivers/HotmailSettingsDisplayDriver.cs
+++ src/OrchardCoreContrib.Email.Hotmail/Drivers/HotmailSettingsDisplayDriver.cs
@@ -7,111 +7,93 @@ using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Environment.Shell;
 using OrchardCore.Settings;
 using OrchardCoreContrib.Email.Hotmail.Services;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace OrchardCoreContrib.Email.Hotmail.Drivers
+namespace OrchardCoreContrib.Email.Hotmail.Drivers;
+
+/// <summary>
+/// Represents a display driver for <see cref="HotmailSettings"/>.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of <see cref="HotmailSettingsDisplayDriver"/>.
+/// </remarks>
+/// <param name="dataProtectionProvider">The <see cref="IDataProtectionProvider"/>.</param>
+/// <param name="shellHost">The <see cref="IShellHost"/>.</param>
+/// <param name="shellSettings">The <see cref="ShellSettings"/>.</param>
+/// <param name="httpContextAccessor">The <see cref="IHttpContextAccessor"/>.</param>
+/// <param name="authorizationService">The <see cref="IAuthorizationService"/>.</param>
+public class HotmailSettingsDisplayDriver(
+    IDataProtectionProvider dataProtectionProvider,
+    IShellHost shellHost,
+    ShellSettings shellSettings,
+    IHttpContextAccessor httpContextAccessor,
+    IAuthorizationService authorizationService) : SectionDisplayDriver<ISite, HotmailSettings>
 {
-    /// <summary>
-    /// Represents a display driver for <see cref="HotmailSettings"/>.
-    /// </summary>
-    public class HotmailSettingsDisplayDriver : SectionDisplayDriver<ISite, HotmailSettings>
+    public const string GroupId = "hotmail";
+
+    /// <inheritdoc/>
+    public override async Task<IDisplayResult> EditAsync(ISite model, HotmailSettings section, BuildEditorContext context)
     {
-        public const string GroupId = "hotmail";
+        var user = httpContextAccessor.HttpContext?.User;
 
-        private readonly IDataProtectionProvider _dataProtectionProvider;
-        private readonly IShellHost _shellHost;
-        private readonly ShellSettings _shellSettings;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IAuthorizationService _authorizationService;
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="HotmailSettingsDisplayDriver"/>.
-        /// </summary>
-        /// <param name="dataProtectionProvider">The <see cref="IDataProtectionProvider"/>.</param>
-        /// <param name="shellHost">The <see cref="IShellHost"/>.</param>
-        /// <param name="shellSettings">The <see cref="ShellSettings"/>.</param>
-        /// <param name="httpContextAccessor">The <see cref="IHttpContextAccessor"/>.</param>
-        /// <param name="authorizationService">The <see cref="IAuthorizationService"/>.</param>
-        public HotmailSettingsDisplayDriver(
-            IDataProtectionProvider dataProtectionProvider,
-            IShellHost shellHost,
-            ShellSettings shellSettings,
-            IHttpContextAccessor httpContextAccessor,
-            IAuthorizationService authorizationService)
+        if (!await authorizationService.AuthorizeAsync(user, HotmailPermissions.ManageHotmailSettings))
         {
-            _dataProtectionProvider = dataProtectionProvider;
-            _shellHost = shellHost;
-            _shellSettings = shellSettings;
-            _httpContextAccessor = httpContextAccessor;
-            _authorizationService = authorizationService;
+            return null;
         }
 
-        /// <inheritdoc/>
-        public override async Task<IDisplayResult> EditAsync(ISite model, HotmailSettings section, BuildEditorContext context)
+        var shapes = new List<IDisplayResult>
         {
-            var user = _httpContextAccessor.HttpContext?.User;
-
-            if (!await _authorizationService.AuthorizeAsync(user, Permissions.ManageHotmailSettings))
+            Initialize<HotmailSettings>("HotmailSettings_Edit", model =>
             {
-                return null;
-            }
+                model.DefaultSender = section.DefaultSender;
+                model.DeliveryMethod = section.DeliveryMethod;
+                model.PickupDirectoryLocation = section.PickupDirectoryLocation;
+                model.Host = section.Host;
+                model.Port = section.Port;
+                model.EncryptionMethod = section.EncryptionMethod;
+                model.AutoSelectEncryption = section.AutoSelectEncryption;
+                model.RequireCredentials = section.RequireCredentials;
+                model.UseDefaultCredentials = section.UseDefaultCredentials;
+                model.UserName = section.UserName;
+                model.Password = section.Password;
+            }).Location("Content:5").OnGroup(GroupId)
+        };
 
-            var shapes = new List<IDisplayResult>
-            {
-                Initialize<HotmailSettings>("HotmailSettings_Edit", model =>
-                {
-                    model.DefaultSender = section.DefaultSender;
-                    model.DeliveryMethod = section.DeliveryMethod;
-                    model.PickupDirectoryLocation = section.PickupDirectoryLocation;
-                    model.Host = section.Host;
-                    model.Port = section.Port;
-                    model.EncryptionMethod = section.EncryptionMethod;
-                    model.AutoSelectEncryption = section.AutoSelectEncryption;
-                    model.RequireCredentials = section.RequireCredentials;
-                    model.UseDefaultCredentials = section.UseDefaultCredentials;
-                    model.UserName = section.UserName;
-                    model.Password = section.Password;
-                }).Location("Content:5").OnGroup(GroupId)
-            };
-
-            if (section?.DefaultSender != null)
-            {
-                shapes.Add(Dynamic("HotmailSettings_TestButton").Location("Actions").OnGroup(GroupId));
-            }
-
-            return Combine(shapes);
+        if (section?.DefaultSender != null)
+        {
+            shapes.Add(Dynamic("HotmailSettings_TestButton").Location("Actions").OnGroup(GroupId));
         }
 
-        /// <inheritdoc/>
-        public override async Task<IDisplayResult> UpdateAsync(ISite model, HotmailSettings section, UpdateEditorContext context)
+        return Combine(shapes);
+    }
+
+    /// <inheritdoc/>
+    public override async Task<IDisplayResult> UpdateAsync(ISite model, HotmailSettings section, UpdateEditorContext context)
+    {
+        var user = httpContextAccessor.HttpContext?.User;
+
+        if (!await authorizationService.AuthorizeAsync(user, HotmailPermissions.ManageHotmailSettings))
         {
-            var user = _httpContextAccessor.HttpContext?.User;
-
-            if (!await _authorizationService.AuthorizeAsync(user, Permissions.ManageHotmailSettings))
-            {
-                return null;
-            }
-
-            if (context.GroupId == GroupId)
-            {
-                var previousPassword = section.Password;
-                await context.Updater.TryUpdateModelAsync(section, Prefix);
-
-                if (string.IsNullOrWhiteSpace(section.Password))
-                {
-                    section.Password = previousPassword;
-                }
-                else
-                {
-                    var protector = _dataProtectionProvider.CreateProtector(nameof(HotmailSettingsConfiguration));
-                    section.Password = protector.Protect(section.Password);
-                }
-
-                await _shellHost.ReleaseShellContextAsync(_shellSettings);
-            }
-
-            return await EditAsync(model, section, context);
+            return null;
         }
+
+        if (context.GroupId == GroupId)
+        {
+            var previousPassword = section.Password;
+            await context.Updater.TryUpdateModelAsync(section, Prefix);
+
+            if (string.IsNullOrWhiteSpace(section.Password))
+            {
+                section.Password = previousPassword;
+            }
+            else
+            {
+                var protector = dataProtectionProvider.CreateProtector(nameof(HotmailSettingsConfiguration));
+                section.Password = protector.Protect(section.Password);
+            }
+
+            await shellHost.ReleaseShellContextAsync(shellSettings);
+        }
+
+        return await EditAsync(model, section, context);
     }
 }
