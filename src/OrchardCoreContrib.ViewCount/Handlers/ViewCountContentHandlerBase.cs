389e2ec6d54824549d0b8c3ee005aeba6d60c9c8
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-05 08:04:44 +0300

    Add docs for ViewCount module

--- src/OrchardCoreContrib.ViewCount/Handlers/ViewCountContentHandlerBase.cs
+++ src/OrchardCoreContrib.ViewCount/Handlers/ViewCountContentHandlerBase.cs
@@ -1,8 +1,13 @@
 ﻿namespace OrchardCoreContrib.ViewCount.Handlers;
 
+/// <summary>
+/// Provides a base implementation for handling view count events for content items.
+/// </summary>
 public abstract class ViewCountContentHandlerBase : IViewCountContentHandler
 {
+    /// <inheritdoc/>
     public virtual Task ViewingAsync(ViewCountContentContext context) => Task.CompletedTask;
 
+    /// <inheritdoc/>
     public virtual Task ViewedAsync(ViewCountContentContext context) => Task.CompletedTask;
 }
