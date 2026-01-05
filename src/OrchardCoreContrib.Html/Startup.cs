861ae9eb4814663a0b98a68d840ac5b48289e224
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-05 10:00:03 +0300

    Bump OCC.Html to 1.4.0

--- src/OrchardCoreContrib.Html/Startup.cs
+++ src/OrchardCoreContrib.Html/Startup.cs
@@ -1,23 +1,35 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
 using OrchardCore.Mvc.Core.Utilities;
+using OrchardCore.ResourceManagement;
 using OrchardCoreContrib.Html.Controllers;
 
-namespace OrchardCoreContrib.Html
+namespace OrchardCoreContrib.Html;
+
+[Feature("OrchardCoreContrib.Html.GrapesJS")]
+public class Startup : StartupBase
 {
-    [Feature("OrchardCoreContrib.Html.GrapesJS")]
-    public class Startup : StartupBase
+    /// <inheritdoc/>
+    public override void ConfigureServices(IServiceCollection services)
     {
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            routes.MapAreaControllerRoute(
-                name: "GrapesJSEditor",
-                areaName: "OrchardCoreContrib.Html",
-                pattern: "GrapesJSEditor",
-                defaults: new { controller = typeof(AdminController).ControllerName(), action = nameof(AdminController.Index) }
-            );
-        }
+        services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
+    }
+
+    /// <inheritdoc/>
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        routes.MapAreaControllerRoute(
+            name: "GrapesJSEditor",
+            areaName: "OrchardCoreContrib.Html",
+            pattern: "GrapesJSEditor",
+            defaults: new
+            {
+                controller = typeof(AdminController).ControllerName(),
+                action = nameof(AdminController.Index)
+            }
+        );
     }
 }
