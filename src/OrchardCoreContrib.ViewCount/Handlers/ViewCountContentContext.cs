f670056ca72764866da4372793280ac429353137
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-05 08:04:44 +0300

    Add docs for ViewCount module

--- src/OrchardCoreContrib.ViewCount/Handlers/ViewCountContentContext.cs
+++ src/OrchardCoreContrib.ViewCount/Handlers/ViewCountContentContext.cs
@@ -3,7 +3,15 @@ using OrchardCore.ContentManagement;
 
 namespace OrchardCoreContrib.ViewCount.Handlers;
 
+/// <summary>
+/// Represents a content context that includes a count of items within a collection.
+/// </summary>
+/// <remarks>Use <see cref="ViewCountContentContext"/> to associate a specific item of content with a count value,
+/// such as the number of times the content has been viewed or the number of related items.</remarks>
 public class ViewCountContentContext(ContentItem contentItem, int count) : ContentContextBase(contentItem)
 {
+    /// <summary>
+    /// Gets or sets the number of items contained in the collection.
+    /// </summary>
     public int Count { get; set; } = count;
 }
--- src/OrchardCoreContrib.ViewCount/Models/ViewCountPart.cs
+++ src/OrchardCoreContrib.ViewCount/Models/ViewCountPart.cs
@@ -1,7 +1,16 @@
 ﻿using OrchardCore.ContentManagement;
 
 namespace OrchardCoreContrib.ViewCount.Models;
+
+/// <summary>
+/// Represents a content part that tracks the number of times an item has been viewed.
+/// </summary>
+/// <remarks>Use <see cref="ViewCountPart"/> to associate a view count with content items, enabling features such
+/// as analytics or popularity tracking.</remarks>
 public class ViewCountPart : ContentPart
 {
+    /// <summary>
+    /// Gets or sets the number of items contained in the collection.
+    /// </summary>
     public int Count { get; set; }
 }
