5d4087351bbdc4848ab23b7270f465edd67b0ee7
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-05 11:29:16 +0300

    Fix GDPR module

---
+++ src/OrchardCoreContrib.Gdpr/GdprPermissions.cs
@@ -0,0 +1,14 @@
+﻿using OrchardCore.Security.Permissions;
+
+namespace OrchardCoreContrib.Gdpr;
+
+/// <summary>
+/// Provides predefined permissions related to the GDPR module.
+/// </summary>
+public class GdprPermissions
+{
+    /// <summary>
+    /// Gets a permission for managing a GDPR settings.
+    /// </summary>
+    public static readonly Permission ManageGdprSettings = new("ManageGdprSettings", "Manage GDPR Settings");
+}
