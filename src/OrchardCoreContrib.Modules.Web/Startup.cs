c1a79fd9c846cd5cbdb5fd8e1959e6e86874ff98
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-06 12:30:43 +0300

    Fix IOE Synchronous operations are disallowed

--- src/OrchardCoreContrib.Modules.Web/Startup.cs
+++ src/OrchardCoreContrib.Modules.Web/Startup.cs
@@ -1,41 +1,40 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using OrchardCore.Environment.Shell;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using OrchardCoreContrib.Users.Services;
 
-namespace OrchardCoreContrib.Modules.Web
+namespace OrchardCoreContrib.Modules.Web;
+
+public class Startup
 {
-    public class Startup
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddOrchardCms(builder =>
-                {
-                    builder.AddSetupFeatures("OrchardCore.AutoSetup", "OrchardCoreContrib.Tenants");
-                    //builder.ConfigureServices(builderServices =>
-                    //{
-                    //    builderServices.AddYesSqlDataMigrations();
+        // Fix IOE Synchronous operations are disallowed
+        services.Configure<KestrelServerOptions>(options => options.AllowSynchronousIO = true);
+        services.Configure<IISServerOptions>(options => options.AllowSynchronousIO = true);
 
-                    //    builderServices.AddScoped<MigrationUpdater>();
-                    //    builderServices.AddScoped<IFeatureEventHandler>(sp => sp.GetRequiredService<MigrationUpdater>());
-                    //});
-                });
-
-            // Workaround to avoid IOE on UserMenu shape
-            services.AddScoped<IAvatarService, NullAvatarService>();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+        services
+            .AddOrchardCms(builder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                builder.AddSetupFeatures("OrchardCore.AutoSetup", "OrchardCoreContrib.Tenants");
+                //builder.ConfigureServices(builderServices =>
+                //{
+                //    builderServices.AddYesSqlDataMigrations();
 
-            app.UseOrchardCore();
+                //    builderServices.AddScoped<MigrationUpdater>();
+                //    builderServices.AddScoped<IFeatureEventHandler>(sp => sp.GetRequiredService<MigrationUpdater>());
+                //});
+            });
+
+        // Workaround to avoid IOE on UserMenu shape
+        services.AddScoped<IAvatarService, NullAvatarService>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseOrchardCore();
     }
 }
