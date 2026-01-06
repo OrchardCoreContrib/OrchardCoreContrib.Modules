5a725cf96ac0f755d01eea1002b78fd821674a6d
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-06 12:30:43 +0300

    Fix IOE Synchronous operations are disallowed

--- src/OrchardCoreContrib.Diagnostics.Elm/Startup.cs
+++ src/OrchardCoreContrib.Diagnostics.Elm/Startup.cs
@@ -3,26 +3,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
-using System;
 
-namespace OrchardCoreContrib.Diagnostics.Elm
+namespace OrchardCoreContrib.Diagnostics.Elm;
+
+/// <summary>
+/// Represensts a startup point to register the required services by Elm diagnostics module.
+/// </summary>
+public class Startup : StartupBase
 {
-    /// <summary>
-    /// Represensts a startup point to register the required services by Elm diagnostics module.
-    /// </summary>
-    public class Startup : StartupBase
+    /// <inheritdoc/>
+    public override void ConfigureServices(IServiceCollection services)
     {
-        /// <inheritdoc/>
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            services.AddElm(options => options.Path = new PathString("/elm"));
-        }
+        services.AddElm(options => options.Path = new PathString("/elm"));
+    }
 
-        /// <inheritdoc/>
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            builder.UseElmCapture();
-            builder.UseElmPage();
-        }
+    /// <inheritdoc/>
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        builder.UseElmPage();
     }
 }
