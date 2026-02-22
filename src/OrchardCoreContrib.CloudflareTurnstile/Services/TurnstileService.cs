ed3681b9db081d866f4da0d81e8f5544ff2040d9
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-02-23 00:21:43 +0300

    Add Cloudflare Turnstile Captcha module

---
+++ src/OrchardCoreContrib.CloudflareTurnstile/Services/TurnstileService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using OrchardCoreContrib.CloudflareTurnstile.Configuration;
+using System.Net.Http.Json;
+
+namespace OrchardCoreContrib.CloudflareTurnstile.Services;
+
+public class TurnstileService(
+    IHttpClientFactory httpClientFactory,
+    IOptions<TurnstileOptions> options)
+{
+    private readonly TurnstileOptions options = options.Value;
+
+    public async Task<bool> ValidateAsync(string token)
+    {
+        var client = httpClientFactory.CreateClient();
+        var content = new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            ["secret"] = options.SecretKey,
+            ["response"] = token
+        });
+
+        var response = await client.PostAsync(Constants.TurnstileApiUri, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadFromJsonAsync<TurnstileResponse>();
+            
+            return json.Success;
+        }
+
+        return false;
+    }
+}
