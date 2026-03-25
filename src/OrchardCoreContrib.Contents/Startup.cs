4dae291f4db8c7165db82d37834677dbd4945be6
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-03-25 10:27:47 +0300

    Add permissions

--- src/OrchardCoreContrib.Contents/Startup.cs
+++ src/OrchardCoreContrib.Contents/Startup.cs
@@ -3,6 +3,7 @@ using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.Data;
 using OrchardCore.Data.Migration;
 using OrchardCore.Modules;
+using OrchardCore.Security.Permissions;
 using OrchardCoreContrib.Contents.Drivers;
 using OrchardCoreContrib.Contents.Indexes;
 using OrchardCoreContrib.Contents.Services;
@@ -21,5 +22,6 @@ public sealed class Startup : StartupBase
         services.AddIndexProvider<SharedDraftLinkIndexProvider>();
 
         services.AddScoped<IContentDisplayDriver, ShareDraftContentDriver>();
+        services.AddPermissionProvider<Permissions>();
     }
 }
