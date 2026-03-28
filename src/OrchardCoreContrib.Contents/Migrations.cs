3879b55f13e7165d8117a66fd42d8900b2170a26
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-03-29 00:32:08 +0300

    Fix SharedDraftLink indexes

--- src/OrchardCoreContrib.Contents/Migrations.cs
+++ src/OrchardCoreContrib.Contents/Migrations.cs
@@ -1,10 +1,11 @@
 ﻿using OrchardCore.Data.Migration;
+using OrchardCore.Environment.Shell;
 using OrchardCoreContrib.Contents.Indexes;
 using YesSql.Sql;
 
 namespace OrchardCoreContrib.Contents;
 
-public class Migrations : DataMigration
+public class Migrations(IShellFeaturesManager shellFeaturesManager) : DataMigration
 {
     public async Task<int> CreateAsync()
     {
@@ -17,8 +18,24 @@ public class Migrations : DataMigration
             .Column<DateTime>("CreatedUtc")
         );
 
+        return 1;
+    }
+
+    public async Task<int> UpdateFrom1Async()
+    {
+        var features = await shellFeaturesManager.GetEnabledFeaturesAsync();
+
+        if (!features.Any(f => f.Id == "OrchardCore.Notifications"))
+        {
+            await SchemaBuilder.AlterIndexTableAsync<SharedDraftLinkIndex>(table =>
+            {
+                table.DropIndex("IDX_NotificationIndex_DocumentId");
+                table.DropIndex("IDX_NotificationIndex_CreatedBy");
+            });
+        }
+
         await SchemaBuilder.AlterIndexTableAsync<SharedDraftLinkIndex>(table => table
-            .CreateIndex("IDX_NotificationIndex_DocumentId",
+            .CreateIndex("IDX_SharedDraftLinkIndex_DocumentId",
                 "DocumentId",
                 "LinkId",
                 "ContentItemId",
@@ -29,13 +46,13 @@ public class Migrations : DataMigration
         );
 
         await SchemaBuilder.AlterIndexTableAsync<SharedDraftLinkIndex>(table => table
-            .CreateIndex("IDX_NotificationIndex_CreatedBy",
+            .CreateIndex("IDX_SharedDraftLinkIndex_CreatedBy",
                 "DocumentId",
                 "ContentItemId",
                 "CreatedBy",
                 "CreatedUtc")
         );
 
-        return 1;
+        return 2;
     }
 }
