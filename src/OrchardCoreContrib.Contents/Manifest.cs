b4aeec81ec5ad047a7c8bfcb143caa3b318130b9
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-03-29 08:25:56 +0300

    Bump OCC.Content to 1.0.1

--- src/OrchardCoreContrib.Contents/Manifest.cs
+++ src/OrchardCoreContrib.Contents/Manifest.cs
@@ -5,7 +5,7 @@ using ManifestConstants = OrchardCoreContrib.Modules.Manifest.ManifestConstants;
     Name = "Contents",
     Author = ManifestConstants.Author,
     Website = ManifestConstants.Website,
-    Version = "1.0.0",
+    Version = "1.0.1",
     Category = "Content Management"
 )]
 
