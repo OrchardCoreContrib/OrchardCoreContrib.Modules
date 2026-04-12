dd752e63049d46729084f1198ea6fd1b9ad78785
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-04-12 21:01:43 +0300

    Add health checks rate limiting feature

---
+++ src/OrchardCoreContrib.HealthChecks/HealthChecksRateLimitingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading.RateLimiting;
+
+namespace OrchardCoreContrib.HealthChecks;
+
+public class HealthChecksRateLimitingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly HealthChecksOptions _healthChecksOptions;
+    private readonly SlidingWindowRateLimiter _rateLimiter;
+    private readonly ILogger _logger;
+
+    public HealthChecksRateLimitingMiddleware(
+        RequestDelegate next,
+        IOptions<HealthChecksOptions> healthChecksOptions,
+        IOptions<HealthChecksRateLimitingOptions> healthChecksRateLimitingOptions,
+        ILogger<HealthChecksRateLimitingMiddleware> logger)
+    {
+        var healthChecksRateLimitingOptionsValue = healthChecksRateLimitingOptions.Value;
+        _rateLimiter = new(new SlidingWindowRateLimiterOptions
+        {
+            PermitLimit = healthChecksRateLimitingOptionsValue.PermitLimit,
+            Window = healthChecksRateLimitingOptionsValue.Window,
+            SegmentsPerWindow = healthChecksRateLimitingOptionsValue.SegmentsPerWindow,
+            QueueLimit = healthChecksRateLimitingOptionsValue.QueueLimit,
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst
+        });
+        _next = next;
+        _healthChecksOptions = healthChecksOptions.Value;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Path.Equals(_healthChecksOptions.Url))
+        {
+            var rateLimitLease = _rateLimiter.AttemptAcquire(1);
+
+            if (!rateLimitLease.IsAcquired)
+            {
+                _logger.LogWarning("Rate limit exceeded for IP Address {RemoteIP}.", context.Connection.RemoteIpAddress);
+
+                context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+
+                await context.Response.WriteAsync("Too Many Requests.");
+
+                return;
+            }
+        }
+
+        await _next(context);
+    }
+}
---
+++ src/OrchardCoreContrib.HealthChecks/HealthChecksRateLimitingOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OrchardCoreContrib.HealthChecks;
+
+public class HealthChecksRateLimitingOptions
+{
+    public int PermitLimit { get; set; } = 5;
+
+    public TimeSpan Window { get; set; } = TimeSpan.FromSeconds(10);
+
+    public int SegmentsPerWindow { get; set; } = 10;
+
+    public int QueueLimit { get; set; } = 0;
+}
+
