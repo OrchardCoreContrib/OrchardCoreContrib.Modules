b155b9e5d92f624fae0037dafb6d344b40e4d092
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-04-23 01:52:22 +0300

    Add missing Scalar UI manifest metadata

--- src/OrchardCoreContrib.Apis.Scalar/Manifest.cs
+++ src/OrchardCoreContrib.Apis.Scalar/Manifest.cs
@@ -2,6 +2,9 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
     Name = "Scalar UI",
+    Author = ManifestConstants.Author,
+    Website = ManifestConstants.Website,
+    Version = "1.0.0",
     Description = "Enables Scalar UI for Orchard Core APIs.",
     Dependencies = ["OrchardCoreContrib.Apis.Swagger"]
 )]
