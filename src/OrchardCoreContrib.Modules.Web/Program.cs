4c8606cf1a63e6187a92f55f815358ae45c3cced
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-05-23 22:25:41 +0300

    Remove startup

--- src/OrchardCoreContrib.Modules.Web/Program.cs
+++ src/OrchardCoreContrib.Modules.Web/Program.cs
@@ -1,20 +1,37 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using OrchardCore.Environment.Shell;
+using OrchardCoreContrib.Avatars;
+using OrchardCoreContrib.Modules.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddOrchardCms(builder =>
+    {
+        builder.AddSetupFeatures("OrchardCore.AutoSetup", "OrchardCoreContrib.Tenants");
+        builder.ConfigureServices(builderServices =>
+        {
+            builderServices.AddYesSqlDataMigrations();
+    
+            builderServices.AddScoped<MigrationUpdater>();
+            builderServices.AddScoped<IFeatureEventHandler>(sp => sp.GetRequiredService<MigrationUpdater>());
+        });
+    });
+
+// Could be add to services.AddOrchardCms().WithOrchardCoreContrib()
+builder.Services.AddScoped<IAvatarService, NullAvatarService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseOrchardCore();
+
+await app.RunAsync();
 
 namespace OrchardCoreContrib.Modules.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    public partial class Program;
 }
