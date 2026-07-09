889f6f43749d5b47052a1566941bac3698b8166a
davidborg-tech <80062143+davidborg-tech@users.noreply.github.com>
2026-07-09 13:28:35 +0300

    Fix Cloudflare Turnstile SiteVerify Api
    Fixed Cloudflare siteverify API url as per https://developers.cloudflare.com/turnstile/get-started/server-side-validation/#siteverify-api-overview

--- src/OrchardCoreContrib.CloudflareTurnstile/Constants.cs
+++ src/OrchardCoreContrib.CloudflareTurnstile/Constants.cs
@@ -4,7 +4,7 @@ public static class Constants
 {
     public const string TurnstileScriptUri = "https://challenges.cloudflare.com/turnstile/v0/api.js";
 
-    public const string TurnstileApiUri = "https://challenges.cloudflare.com/turnstile/v0/api/";
+    public const string TurnstileApiUri = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
 
     public const string TurnstileServerResponseHeaderName = "cf-turnstile-response";
 }
