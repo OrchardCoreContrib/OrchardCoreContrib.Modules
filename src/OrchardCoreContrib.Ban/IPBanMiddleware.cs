d79cef7de6e6d3fc865dc3ec7a37853604750e5c
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-07-14 06:33:18 +0300

    Don't skip admin requests

--- src/OrchardCoreContrib.Ban/IPBanMiddleware.cs
+++ src/OrchardCoreContrib.Ban/IPBanMiddleware.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
 using OrchardCoreContrib.Ban.Services;
 
 namespace OrchardCoreContrib.Ban;
@@ -10,7 +8,6 @@ namespace OrchardCoreContrib.Ban;
 public class IPBanMiddleware(
     RequestDelegate next,
     IIPBanService ipBanService,
-    IOptions<AdminOptions> adminOptions,
     ILogger<IPBanMiddleware> logger)
 {
     public async Task InvokeAsync(HttpContext context)
@@ -24,13 +21,6 @@ public class IPBanMiddleware(
             return;
         }
 
-        // Skip admin requests
-        if (context.Request.Path.StartsWithSegments($"/{adminOptions.Value.AdminUrlPrefix}"))
-        {
-            await next(context);
-
-            return;
-        }
 
         var ip = context.Connection.RemoteIpAddress;
         if (await ipBanService.IsBannedAsync(ip))
